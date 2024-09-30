f5805a21bfea0a7c5e8747b0644c5bacbda487bf
안동현 <coner232@hotmail.com>
2024-09-30 17:14:45 +0900

    no message

---
+++ workspace_java/proj11_ajax/src/main/java/human/class1/ajax/dto/MemberDTO.java
@@ -0,0 +1,23 @@
+package human.class1.ajax.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//@Getter
+//@Setter
+//@ToString
+//@RequiredArgsConstructor
+//@EqualsAndHashCode
+@Data
+public class MemberDTO {
+
+	private String userID;
+	private String userPW;
+	private int age;
+	private String userName;
+	
+}
---
+++ workspace_java/proj11_ajax/src/main/java/human/class1/ajax/service/EmpService.java
@@ -0,0 +1,10 @@
+package human.class1.ajax.service;
+
+import java.util.List;
+
+import human.class1.ajax.dto.EmpDTO;
+
+public interface EmpService {
+
+	List<EmpDTO> listEmp();
+}
